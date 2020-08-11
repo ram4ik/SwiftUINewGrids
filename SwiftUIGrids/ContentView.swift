95589db5fe6145873789e35a585b38c72b2bb2a1
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-11 20:34:10 +0300

    Initial Commit

---
+++ SwiftUIGrids/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIGrids
+//
+//  Created by Ramill Ibragimov on 11.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGrids/SwiftUIGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGridsApp.swift
+//  SwiftUIGrids
+//
+//  Created by Ramill Ibragimov on 11.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
